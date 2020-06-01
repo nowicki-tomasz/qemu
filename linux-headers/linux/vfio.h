07017dc0f622b991eff5da2c9cd0303af4f60a1f
Tomasz Nowicki <tn@semihalf.com>
2020-06-01 15:47:33 +0200

    vfio: Extend vfio_get_device() to have insight into the clock numbers
    In order to support clocks for VFIO platform devices, QEMU needs to know
    how many clock is correlated with VFIO device, similarly to region and
    IRQ strategy. Add necessary infrastructure.
    
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>

--- linux-headers/linux/vfio.h
+++ linux-headers/linux/vfio.h
@@ -202,6 +202,7 @@ struct vfio_device_info {
 #define VFIO_DEVICE_FLAGS_CCW	(1 << 4)	/* vfio-ccw device */
 #define VFIO_DEVICE_FLAGS_AP	(1 << 5)	/* vfio-ap device */
 	__u32	num_regions;	/* Max region index + 1 */
+	__u32	num_clks;	/* Max clock index + 1 */
 	__u32	num_irqs;	/* Max IRQ index + 1 */
 };
 #define VFIO_DEVICE_GET_INFO		_IO(VFIO_TYPE, VFIO_BASE + 7)
